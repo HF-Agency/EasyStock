480009b959b95928d194fb2ccbf379fb63e884cf
Henrique Oliveira <henriquefpcoliveira@gmail.com>
2024-03-15 00:23:13 +0000

    Added users-secrets to store connection strings

--- EasyStock.API/EntityFramework/EasyStockContext.cs
+++ EasyStock.API/EntityFramework/EasyStockContext.cs
@@ -6,11 +6,18 @@ namespace EasyStock.API.EntityFramework
 {
     public class EasyStockContext : DbContext
     {
+        private readonly IConfiguration config;
+
+        public EasyStockContext(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         public DbSet<Product> Products { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=EasyStockDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer(config["ConnectionString"]);
         }
     }
 }

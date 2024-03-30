3a3281f0834e5f1aebabf982e4e14b33b0de8c75
Henrique Oliveira <henriquefpcoliveira@gmail.com>
2024-03-30 23:25:01 +0000

    Created and Implemented Product Client Interface 1) Created Product Client Interface; 2) Implemented Create, Update and Delete methods of Product client.

--- EasyStock.App/Services/Clients/ProductClient.cs
+++ EasyStock.App/Services/Clients/ProductClient.cs
@@ -2,10 +2,11 @@
 using System.Net.Http.Json;
 using Microsoft.Extensions.Logging;
 using System;
+using EasyStock.Library.Entities.Interface;
 
 namespace EasyStock.App.Services.Clients
 {
-    public class ProductClient
+    public class ProductClient : IProductClient
     {
         private readonly HttpClient _httpClient;
         private readonly ILogger<ProductClient> _logger;
@@ -15,6 +16,7 @@ namespace EasyStock.App.Services.Clients
             _httpClient = httpClient;
             _logger = logger;
         }
+
 
         public async Task<IEnumerable<Product>?> GetProductsAsync()
         {
@@ -64,5 +66,84 @@ namespace EasyStock.App.Services.Clients
                 return null; // Propagate unexpected exceptions.
             }
         }
+
+        public async Task CreateProductAsync(Product product)
+        {
+            try
+            {
+                _logger.LogInformation("Creating a new product");
+                var response = await _httpClient.PostAsJsonAsync("/product", product);
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Product successfully created");
+                }
+                else
+                {
+                    _logger.LogWarning("Failed to create product. Status code: {StatusCode}", response.StatusCode);
+                }
+                response.EnsureSuccessStatusCode(); // Throws an exception if the HTTP response status is an error code.
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, "Error creating product in API.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred while creating a product.");
+            }
+        }
+
+        public async Task UpdateProductAsync(int productId, Product product)
+        {
+            try
+            {
+                _logger.LogInformation($"Updating product with ID: {productId}");
+                var response = await _httpClient.PutAsJsonAsync($"/product/{productId}", product);
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation($"Product with ID: {productId} successfully updated");
+                }
+                else
+                {
+                    _logger.LogWarning("Failed to update product with ID: {ProductId}. Status code: {StatusCode}", productId, response.StatusCode);
+                }
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, $"Error updating product in API for ID: {productId}.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An unexpected error occurred while updating product details for ID: {productId}.");
+            }
+        }
+
+        public async Task DeleteProductAsync(int productId)
+        {
+            try
+            {
+                _logger.LogInformation($"Deleting product with ID: {productId}");
+                var response = await _httpClient.DeleteAsync($"/product/{productId}");
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation($"Product with ID: {productId} successfully deleted");
+                }
+                else
+                {
+                    _logger.LogWarning("Failed to delete product with ID: {ProductId}. Status code: {StatusCode}", productId, response.StatusCode);
+                }
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, $"Error deleting product from API for ID: {productId}.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An unexpected error occurred while deleting product for ID: {productId}.");
+            }
+        }
+
     }
 }
---
+++ EasyStock.Library/Entities/Interface/IProductClient.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyStock.Library.Entities.Interface
+{
+    public interface IProductClient
+    {
+        Task<IEnumerable<Product>?> GetProductsAsync();
+        Task<Product?> GetProductByIdAsync(int productId);
+        Task CreateProductAsync(Product product);
+        Task UpdateProductAsync(int productId, Product product);
+        Task DeleteProductAsync(int productId);
+    }
+}
